b35b53810b7381171ae72691a0f7a4b908410d48
rymbln <rymbln@gmail.com>
2019-09-13 12:04:07 +0300

    turn off https

--- Startup.cs
+++ Startup.cs
@@ -46,10 +46,10 @@ namespace dummy_api
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
